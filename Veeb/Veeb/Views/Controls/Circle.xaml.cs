60a6f4134985f4a257933f6e66b782d3cd1760ab
DESKTOP-RKA0J95\micro <microsoftaccayntstas04@gmail.com>
2020-12-13 00:56:30 +0300

    i don't know

---
+++ Veeb/Veeb/Views/Controls/Circle.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veeb.Models;
+using Veeb.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Veeb.Views.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Circle : ContentView
+    {
+        public static BindableProperty CurrentStateProperty =
+            BindableProperty.Create(nameof(CurrentState), typeof(State),
+                typeof(Circle), default(State), propertyChanged: OnCurrentStatePropertyChanged);
+        public static BindableProperty StateProperty =
+            BindableProperty.Create(nameof(State), typeof(State),
+                typeof(Circle), default(State));
+
+        public Circle()
+        {
+            InitializeComponent();
+        }
+
+        public State CurrentState
+        {
+            get => (State)GetValue(CurrentStateProperty);
+            set => SetValue(CurrentStateProperty, value);
+        }
+
+        public State State
+        {
+            get => (State)GetValue(StateProperty);
+            set => SetValue(StateProperty, value);
+        }
+
+        private static void OnCurrentStatePropertyChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            var circle = bindable as Circle;
+            circle.Ellipse.Fill = ((State)newvalue) == circle.State ? Colors.OnColor : Colors.OffColor;
+        }
+    }
+}
